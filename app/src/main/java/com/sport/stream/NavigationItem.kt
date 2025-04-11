96accab4d13b849ec3f91d612efca174018df531
KimtungIT <kimtungit@gmail.com>
2025-04-11 13:40:00 +0700

    init

---
+++ app/src/main/java/com/sport/stream/NavigationItem.kt
@@ -0,0 +1,8 @@
+package com.sport.stream
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val title: String,
+    val icon: ImageVector,
+    val route: String
+)
